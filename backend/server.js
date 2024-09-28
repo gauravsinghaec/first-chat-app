03a95c5dd180782f34f77ca72ccf1ac548fe71ad
Gaurav Singh <gauravsingh@Gauravs-Laptop.local>
2024-09-28 17:48:45 +0530

    Backend setup with socket.io

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("sendMessage", (message) => {
+    io.emit("receiveMessage", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+const port = 4000;
+// const port = process.env.PORT;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
